cb82eefa7a6124cdbdcdad44793fce20db0eb118
GnohiSiaM <mrhellomonday@gmail.com>
2016-12-21 14:45:46 +0800

    java(spring-boot-demo): test maven profiles

--- java/spring-boot-demo/src/main/java/com/gnohisiam/demo/configuration/TestProperties.java
+++ java/spring-boot-demo/src/main/java/com/gnohisiam/demo/configuration/TestProperties.java
@@ -3,9 +3,11 @@ package com.gnohisiam.demo.configuration;
 import lombok.Data;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
+@PropertySource("classpath:test.properties")
 @Component
 @ConfigurationProperties(prefix = "test")
 public class TestProperties {
