f3f5f7b0ce1a16557cc258fcd685c54f68ceea08
GnohiSiaM <mrhellomonday@gmail.com>
2017-01-04 20:14:32 +0800

    java(spring-boot-demo): remove spring-boot-admin-server

--- java/spring-boot-demo/src/main/java/com/gnohisiam/demo/Application.java
+++ java/spring-boot-demo/src/main/java/com/gnohisiam/demo/Application.java
@@ -1,12 +1,9 @@
 package com.gnohisiam.demo;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class Application {
 
     public static void main(String[] args) {
