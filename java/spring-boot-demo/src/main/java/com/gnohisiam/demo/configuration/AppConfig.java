deb73ba14b4477f62e3942c79d05c2f7b8497138
GnohiSiaM <mrhellomonday@gmail.com>
2017-01-17 15:11:19 +0800

    java(spring-boot-demo): enable mongo auditing

--- java/spring-boot-demo/src/main/java/com/gnohisiam/demo/configuration/AppConfig.java
+++ java/spring-boot-demo/src/main/java/com/gnohisiam/demo/configuration/AppConfig.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableAsync
 @EnableScheduling
+@EnableMongoAuditing
 public class AppConfig extends AsyncConfigurerSupport {
 
     @Override
--- java/spring-boot-demo/src/main/java/com/gnohisiam/demo/model/BaseModel.java
+++ java/spring-boot-demo/src/main/java/com/gnohisiam/demo/model/BaseModel.java
@@ -1,5 +1,8 @@
 package com.gnohisiam.demo.model;
 
+import static com.gnohisiam.demo.common.Constants.GMT_8;
+import static com.gnohisiam.demo.common.Constants.TIME_PATTERN;
+
 import java.util.Date;
 
 import lombok.Data;
@@ -8,6 +11,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Data
@@ -17,8 +21,10 @@ public class BaseModel {
     @Id
     private String id;
     @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIME_PATTERN, timezone = GMT_8)
     private Date created;
     @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIME_PATTERN, timezone = GMT_8)
     private Date lastModified;
     @Version
     private Long version;
