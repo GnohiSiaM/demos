b9af535cd86b33eb310aa13bea14926b5283dc32
GnohiSiaM <mrhellomonday@gmail.com>
2017-01-18 12:46:28 +0800

    java(spring-boot-demo): set default time zone

--- java/spring-boot-demo/src/main/java/com/gnohisiam/demo/Application.java
+++ java/spring-boot-demo/src/main/java/com/gnohisiam/demo/Application.java
@@ -1,5 +1,7 @@
 package com.gnohisiam.demo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         SpringApplication.run(Application.class, args);
     }
 
